da84c085dcdb83b69b26351d82e2433cce47ce1e
Footanari <nigslayrr@gmail.com>
2023-03-10 22:56:07 +0200

    added bluetooth.ino

---
+++ arduino-code/bluetooth.ino
@@ -0,0 +1,39 @@
+#include <ArduinoBLE.h>
+
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214");
+
+void btSetup() {
+  Serial.begin(9600);
+
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("baken_paken");
+
+  BLE.addService(ledService);
+
+  BLE.advertise();
+}
+
+void btUpdate() {
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+
+    while (central.connected());
+
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void setup() {
+  btSetup();
+}
+void loop() {
+  btLoop();
+}
